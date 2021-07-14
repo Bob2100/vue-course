60f7d76eb87561872e0f10d98fc096c759f466d6
Bob <bob_w@qq.com>
2021-07-14 13:46:37 +0800

    test

--- data-structure/test.js
+++ data-structure/test.js
@@ -7,5 +7,5 @@ assert('test append and size', list.size() === 1);
 assert('test findIndex', list.findIndex('Bob') === 0);
 
 list.remove('Bob');
-assert('test remove', list.size() === 1);
+assert('test remove', list.size() === 0);
 assert('test toString', Array.isArray(list.toString()));
