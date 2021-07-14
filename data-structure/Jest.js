92ebeed952a1df4ba4536d4bd01171f747730d77
Bob <bob_w@qq.com>
2021-07-14 11:20:57 +0800

    test list

--- data-structure/Jest.js
+++ data-structure/Jest.js
@@ -1,12 +1,12 @@
 const style = {
   red: '\x1B[31m',
-  green: '\x1B[32m'
+  green: '\x1B[32m',
 }
 function assert(desc, condition) {
   if (!condition) {
     console.log(style.red);
     throw new Error(desc);
   }
-  console.log(style.green,`${desc}: passed`);
+  console.log(style.green, `${desc}: passed`);
 }
 module.exports = { assert };
--- data-structure/test.js
+++ data-structure/test.js
@@ -3,5 +3,9 @@ const { assert } = require('./Jest');
 
 const list = new List();
 list.append('Bob');
-assert('test size',list.size() === 1);
+assert('test append and size', list.size() === 1);
 assert('test findIndex', list.findIndex('Bob') === 0);
+
+list.remove('Bob');
+assert('test remove', list.size() === 0);
+assert('test toString', list.toString !== null);
